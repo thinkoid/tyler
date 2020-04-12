678f012cd45718ffd9575cd6c2d4e62c7ffdc81b
Liviu Nicoara <lnicoara at thinkoid dot org>
2020-04-12 18:21:47 -0400

    SIGCHLD handler.

--- tyler.c
+++ tyler.c
@@ -3,6 +3,27 @@
 #include <defs.h>
 #include <display.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+/**********************************************************************/
+
+xstatic void sigchld_handler(int ignore) {
+        UNUSED(ignore);
+        while (0 < waitpid (-1, 0, WNOHANG)) ;
+}
+
+static void setup_sigchld() {
+    if (SIG_ERR == signal(SIGCHLD, sigchld_handler)) {
+            fprintf(stderr, "wm : failed to install SIGCHLD handler\n");
+            exit(1);
+    }
+}
+
+/**********************************************************************/
+
 int main ()
 {
         Display* dpy;
@@ -14,6 +35,8 @@ int main ()
         ASSERT (dpy);
 
         atexit(&free_display);
+
+        setup_sigchld();
 
         return 0;
 }
