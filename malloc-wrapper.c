20cc6965f60fbf01ad639c339658f6e926ca6a12
Liviu Nicoara <lnicoara at thinkoid dot org>
2020-05-24 10:42:34 -0400

    Typefarts.

--- malloc-wrapper.c
+++ malloc-wrapper.c
@@ -1,13 +1,16 @@
 /* -*- mode: c; -*- */
 
-#include <memory.h>
+#include <malloc-wrapper.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 void *malloc_(size_t n)
 {
         void *p = malloc(n);
 
         if (0 == p) {
-                fprintf("malloc failed : %lu bytes\n", n);
+                fprintf(stderr, "malloc failed : %lu bytes\n", n);
                 exit(1);
         }
 
